bc5c14e4502eeb8058b75f68a6010c56dd6df7b3
daisytorres <daisytorres.cal@gmail.com>
2023-09-06 00:57:22 -0700

    initial

---
+++ Models/Users.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel.DataAnnotations;
+namespace FormSubmission.Models;
+
+public class User
+{
+    [Required]
+    [MinLength(2, ErrorMessage = "Name must be at least 2 characters long")]
+    public string Name {get;set;}
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email Address")]
+    public string Email {get;set;}
+    [Required]
+    [DataType(DataType.Date)]
+    [PastDate]
+    public DateTime DoB {get;set;}
+    [Required]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+    public string Password {get;set;}
+    [Required]
+    [Display(Name = "Favorite Odd Number")]
+    [OddNumber]
+    public int FavOddNumber {get;set;}
+}
+
+public class PastDateAttribute : ValidationAttribute
+{ 
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)    
+    {
+        if (((DateTime)value) > DateTime.Now)
+        {  
+            return new ValidationResult("Date must be in the past!");   
+        } else {   
+            return ValidationResult.Success;  
+        }  
+    }
+}
+
+public class OddNumberAttribute : ValidationAttribute //set it to inherit from ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if((int)value % 2 == 0) //how to find out if something is an odd number
+        {
+            return new ValidationResult("Number must be odd!");
+        } else {
+            return ValidationResult.Success;
+        }
+    }
+}
